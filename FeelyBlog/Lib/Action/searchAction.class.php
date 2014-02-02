cd6235d93ea277a55b04bb20525bf7699679ffa8
zyzsdy <zyzsdy@gmail.com>
2014-02-02 14:10:21 +0800

    修改bilibili嵌入：播放器调用代码修改，网页替换表达式修改，播放器样式修改。
    Signed-off-by: zyzsdy <zyzsdy@gmail.com>

--- FeelyBlog/Lib/Action/searchAction.class.php
+++ FeelyBlog/Lib/Action/searchAction.class.php
@@ -31,15 +31,17 @@ class searchAction extends Action {
         curl_setopt($link, CURLOPT_URL, $url);
         curl_setopt($link, CURLOPT_PORT, 80);
         curl_setopt($link, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($link, CURLOPT_USERAGENT, "FeelyBlog/beta 1.0 (zyzsdy@gmail.com)");
+        curl_setopt($link, CURLOPT_USERAGENT, "FeelyBlog/beta 1.1 (zyzsdy@gmail.com)");
         $result=curl_exec($link);
         curl_close($link);
         
         $info=json_decode($result);
         
         //$htmlTpl="<embed style=\"width:970px;height:536px;\" pluginspage=\"http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash\" allowfullscreeninteractive=\"true\" flashvars=\"cid=%s&aid=%s\" src=\"https://static-s.bilibili.tv/play.swf\" type=\"application/x-shockwave-flash\" allowscriptaccess=\"always\" allowfullscreen=\"true\" quality=\"high\">";
-        $htmlTpl="<iframe height=\"536\" width=\"970\" src=\"https://secure.bilibili.tv/secure,cid=%s&aid=%s\" scrolling=\"no\" border=\"0\" frameborder=\"no\" framespacing=\"0\" onload=\"window.securePlayerFrameLoaded=true\"></iframe>";
-        
+        //$htmlTpl="<iframe height=\"536\" width=\"970\" src=\"https://secure.bilibili.tv/secure,cid=%s&aid=%s\" scrolling=\"no\" border=\"0\" frameborder=\"no\" framespacing=\"0\" onload=\"window.securePlayerFrameLoaded=true\"></iframe>";
+        //$htmlTpl="<iframe src=\"javascript:'&lt;embed id=&quot;bofqi_embed&quot; style=&quot;width:100%;height:100%;position:absolute;top:0;left:0;&quot; pluginspage=&quot;http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash&quot; allowfullscreeninteractive=&quot;true&quot; flashvars=&quot;cid=%s&amp;aid=%s&quot; src=&quot;https://static-s.bilibili.tv/play.swf&quot; type=&quot;application/x-shockwave-flash&quot; allowscriptaccess=&quot;always&quot; allowfullscreen=&quot;true&quot; quality=&quot;high&quot;&gt;'\" style=\"border:none;\"></iframe>";
+        $htmlTpl="<iframe src=\"javascript:'<embed id=bofqi_embed style=width:100%%;height:100%%;position:absolute;top:0;left:0; pluginspage=http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash allowfullscreeninteractive=true flashvars=cid=%s&aid=%s&as_wide=1 src=https://static-s.bilibili.tv/play.swf type=application/x-shockwave-flash allowscriptaccess=always allowfullscreen=true quality=high>'\" style=\"border:none;width:950px;height:588px\"></iframe>";
+
         if($info->code!=-403&&$info->code!=-404&&$info->code!=-400){
             printf($htmlTpl,$info->cid,$avid);
         }else{
